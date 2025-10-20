9dff1e64de5c2af99d752571051e81e20d510246
pasibun <matthijs_hovestad@hotmail.com>
2025-10-20 12:24:22 +0200

    feat: update index.ts to include GeometryTypes and DocumentTypes in type exports

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -2,8 +2,7 @@
 export { useChecker } from './store';
 export { spectralLinter } from './spectral';
 export type { Rulesets } from './spectral';
-export type { Spec, SpecLinter, SpecInput, SpecResponseMapper, Severity, Diagnostic } from './types';
-export { GeometryTypes, DocumentTypes } from './types';
+export type { Spec, SpecLinter, SpecInput, SpecResponseMapper, Severity, Diagnostic, GeometryTypes, DocumentTypes } from './types';
 export { groupBy, groupBySource, handleResponse, handleResponseJson, formatDocument } from './util';
 export { APPLICATION_JSON_TYPE, APPLICATION_GEO_JSON_TYPE, APPLICATION_OPENAPI_JSON_3_0_TYPE } from './constants';
 export type { OpenAPIV3_0 } from './openapi-types';
