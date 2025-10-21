d0802b541fa543e8325f83be5c244f06f1ab87ce
pasibun <matthijs_hovestad@hotmail.com>
2025-10-21 19:54:34 +0200

    fix: reorder spectral-parsers dependency and update zustand version

--- dist/ui/router.d.ts
+++ dist/ui/router.d.ts
@@ -1,4 +1,4 @@
 import type { Spec } from './types';
-export declare const createRouter: (specs: Spec[]) => import("react-router-dom").DataRouter;
+export declare const createRouter: (specs: Spec[]) => import("react-router").DataRouter;
 export default createRouter;
 //# sourceMappingURL=router.d.ts.map
